3cd675cf1e0441f103a0055fb98958583a5dd93c
Manuel Espejo <manuel.a.espejo@gmail.com>
2021-04-14 12:23:56 +0200

    Version 1.0

--- src/core/servers/express.server.ts
+++ src/core/servers/express.server.ts
@@ -6,7 +6,7 @@ export const createApp = () => {
   const app = express();
   app.use(cors({ origin: envConstants.CORS_ORIGIN }));
 
-  app.use(express.static('public'));
+  app.use(express.static(__dirname + '(/public'));
   //app.use('/avatar', express.static(__dirname + '/public/avatar'));
 
   var bodyParser = require('body-parser');
